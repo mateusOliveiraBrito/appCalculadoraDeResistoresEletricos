33b8056c0f49e9f3a0f765e093b1035168181d0e
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-05-02 19:22:11 -0300

    Cria telas para as demais rotinas da aplicação

---
+++ app/src/main/java/br/edu/ifgoiano/projetodefisica/appcalculadoraderesistoreseletricos/CalculosEmParaleloActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifgoiano.projetodefisica.appcalculadoraderesistoreseletricos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculosEmParaleloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculos_em_paralelo);
+    }
+}
